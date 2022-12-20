d963e09b5183d6692d541da317f663c5403428fc
ipjayawick <ipjayawick@gmail.com>
2022-12-20 10:51:40 +0530

    pages created

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 const posts=require('./routes/api/posts')
-app.use('/api/posts',posts)
+app.use('/',posts)
 
 if(process.env.NODE_ENV==='production'){
     app.use(express.static(__dirname+'/public/'))
--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -3,24 +3,25 @@ const router = express.Router();
 const db = require('../../database')
 
 //routes
-router.get('/', async (req, res) => {
+
+router.get('/register', async (req, res) => {
     const list = await db.getList()
     res.send(list)
 })
 
 
-router.post('/', async (req, res) => {
-    const todo_item = req.body.item;
-    if (todo_item!="") {
-        await db.createTodo(todo_item)
-    }
-    res.status(201).send()
-})
+// router.post('/', async (req, res) => {
+//     const todo_item = req.body.item;
+//     if (todo_item!="") {
+//         await db.createTodo(todo_item)
+//     }
+//     res.status(201).send()
+// })
 
-router.delete('/:id', async (req, res) => {
-    const item=req.params.id
-    await db.deleteTodo(item)
-    res.status(200).send()
-})
+// router.delete('/:id', async (req, res) => {
+//     const item=req.params.id
+//     await db.deleteTodo(item)
+//     res.status(200).send()
+// })
 
 module.exports=router;
